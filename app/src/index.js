4c8044c34df6d51fd9c98da7219804ab637e9881
Utkarsh Padia <utkarshpadia@gmail.com>
2022-05-24 14:09:20 +0530

    index call

--- app/src/index.js
+++ app/src/index.js
@@ -43,7 +43,7 @@ const App = {
   lookUp: async function (){
     const { lookUptokenIdToStarInfo } = this.meta.methods;
     const id = document.getElementById('lookid').value;
-    const starName = await lookUptokenIdToStarInfo(id);
+    const starName = await lookUptokenIdToStarInfo(id).call();
     App.setStatus("The star name is "+starName);
 
 
